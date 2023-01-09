cd464517916c2c9d0bfc2bf6461a98cd4a1b9b1e
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-01-09 09:46:17 -0300

    ok

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var menu = document.querySelector('nav ul');
 var menubar = document.querySelector('nav .menu-icon');
-var iconMenu = docu.querySelector('nav .menu')
+var iconMenu = docu.querySelector('nav .menu-icon img');
+
+menubar.addeventlistemer('click' , function(){
+    menu.classList.toggle('active');
+})
